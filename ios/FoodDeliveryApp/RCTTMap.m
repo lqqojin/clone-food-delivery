bbb2766250a087a114928227dbf4e8caeffdcbbc
lqqojin <lqqojin@gmail.com>
2022-08-06 15:28:37 +0900

    ch5.03-IOS 개발 1. 티맵 연동 2. 스플래쉬 아이콘 3. 벡터 아이콘 4. 앱 아이콘

--- ios/FoodDeliveryApp/RCTTMap.m
+++ ios/FoodDeliveryApp/RCTTMap.m
@@ -2,18 +2,26 @@
 #import "TMapTapi.h"
 
 @implementation RCTTMap
-
+- (instancetype)init
+{
+    // 모듈이 로딩될 때 실행되는 부분
+    NSLog(@"모듈이 로딩될 때 실행되는 부분");
+    self = [super init];
+    [TMapTapi setSKTMapAuthenticationWithDelegate:self apiKey:@"l7xx9691298243384a2aa24e1f179ef649db"]; // 여기에 여러분의 키 넣기
+  NSLog(@"%@", self);
+    return self;
+}
 - (dispatch_queue_t)methodQueue
 {
+    // 메인쓰레드만 쓰도록
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE(TMap);
 
 RCT_EXPORT_METHOD(openNavi: (NSString *)name longitude:(NSString *)longitude latitude:(NSString *)latitude vehicle:(NSString *)vehicle resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
-    [TMapTapi setSKTMapAuthenticationWithDelegate:self apiKey:@"l7xx9691298243384a2aa24e1f179ef649db"];
-    NSLog(@"y,x %@, %@", latitude, longitude);
     BOOL installed = [TMapTapi isTmapApplicationInstalled];
     if (installed) {
+        NSLog(@"인슬톨 됨");
         CLLocationCoordinate2D centerCoordinate = {[latitude doubleValue], [longitude doubleValue]};
         BOOL flag = [TMapTapi invokeRoute:name coordinate: centerCoordinate];
         NSLog(flag ? @"Yes" : @"No");
@@ -22,9 +30,10 @@ RCT_EXPORT_METHOD(openNavi: (NSString *)name longitude:(NSString *)longitude lat
         } else {
             resolve(@(NO));
         }
-    } else {
-        resolve(@(NO));
-    }
+   } else {
+       NSLog(@"인슬톨안됨");
+       resolve(@(NO));
+   }
 }
 
 @end
